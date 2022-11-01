abb9efdb31bb98f67dc8b104e32c0c5f5785cb02
gustavofpsilva <gustavofpsilva@outlook.com>
2022-11-01 01:15:49 -0300

    new response

--- assets/js/responses.js
+++ assets/js/responses.js
@@ -7,7 +7,7 @@ function getBotResponse(input) {
     } else if (input == "Tesoura") {
         return "Pedra";
     }
-}
+
     //1
     if (input == "Bom dia") {
         return "Bom dia, como podemos te ajudar?";
@@ -25,30 +25,4 @@ function getBotResponse(input) {
     } else {
         return "Não entendemos a sua dúvida, nos contate no email contato@utfarm.com.br";
     }
-
-    //3
-    if (input == "Teste1") {
-        return "Bom dia, como podemos te ajudar?";
-    } else if (input == "Obrigado") {
-        return "A Utfarm agradece o contato!";
-    } else {
-       return "Não entendemos a sua dúvida, nos contate no email contato@utfarm.com.br";
-    }
-
-
-    //4
-    if (input == "Teste2") {
-        return "Bom dia, como podemos te ajudar?";
-    } else if (input == "Obrigado") {
-        return "A Utfarm agradece o contato!";
-    } else {
-        return "Não entendemos a sua dúvida, nos contate no email contato@utfarm.com.br";
-        }
-    //5
-    if (input == "Teste3") {
-        return "Bom dia, como podemos te ajudar?";
-    } else if (input == "Obrigado") {
-        return "A Utfarm agradece o contato!";
-    } else {
-        return "Não entendemos a sua dúvida, nos contate no email contato@utfarm.com.br";
-    }
+}
