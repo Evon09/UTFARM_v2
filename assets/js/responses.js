9842ef14a3c9bb571ab022cc5e7f25aa2b54a327
gustavofpsilva <gustavofpsilva@outlook.com>
2022-11-01 01:32:39 -0300

    Correction toLowerCase()

--- assets/js/responses.js
+++ assets/js/responses.js
@@ -1,17 +1,17 @@
 function getBotResponse(input) {
     //rock paper scissors
-    if (input.toLowerCase() == "Pedra") {
+    if (input.toLowerCase() == "pedra") {
         return "Papel";
     } else if (input.toLowerCase() == "papel") {
         return "Tesoura";
-    } else if (input.toLowerCase() == "Tesoura") {
+    } else if (input.toLowerCase() == "tesoura") {
         return "Pedra";
     }
-    if (input.toLowerCase() == "Bom dia") {
+    if (input.toLowerCase() == "bom dia") {
         return "Bom dia";
-    } else if (input.toLowerCase()== "Boa tarde") {
+    } else if (input.toLowerCase()== "boa tarde") {
         return "Boa tarde";
-    } else if (input.toLowerCase() == "Boa noite") {
+    } else if (input.toLowerCase() == "boa noite") {
         return "Boa noite";
     }
 }
