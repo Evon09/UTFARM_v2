704872ee7ff2054230c1e2c2978b0f24f7f7b8ed
gustavofpsilva <gustavofpsilva@outlook.com>
2022-11-01 01:37:29 -0300

    New responses chatbot finally

--- assets/js/responses.js
+++ assets/js/responses.js
@@ -8,10 +8,17 @@ function getBotResponse(input) {
         return "Pedra";
     }
     if (input.toLowerCase() == "bom dia") {
-        return "Bom dia";
+        return "Bom dia, como podemos te ajudar?";
     } else if (input.toLowerCase()== "boa tarde") {
-        return "Boa tarde";
+        return "Boa tarde, como podemos te ajudar?";
     } else if (input.toLowerCase() == "boa noite") {
-        return "Boa noite";
+        return "Boa noite, como podemos te ajudar?";
+    }
+    if (input.toLowerCase() == "quais são os produtos?") {
+        return "Os produtos são...";
+    } else if (input.toLowerCase()== "qual o contato?") {
+        return "O nosso contato é contato@utfarm.com.br";
+    } else if (input.toLowerCase() == "qual o endereço?") {
+        return "Nós ficamos localizados em...";
     }
 }
