725dd95bcba389ce98a9c33b29750f72f901c9a8
gustavofpsilva <gustavofpsilva@outlook.com>
2022-11-01 01:01:48 -0300

    New Responses

--- assets/js/responses.js
+++ assets/js/responses.js
@@ -7,6 +7,7 @@ function getBotResponse(input) {
     } else if (input == "Tesoura") {
         return "Pedra";
     }
+}
     //1
     if (input == "Bom dia") {
         return "Bom dia, como podemos te ajudar?";
@@ -15,6 +16,7 @@ function getBotResponse(input) {
     } else {
         return "Não entendemos a sua dúvida, nos contate no email contato@utfarm.com.br";
     }
+
     //2
     if (input == "Bom dia") {
          return "Bom dia, como podemos te ajudar?";
@@ -23,16 +25,19 @@ function getBotResponse(input) {
     } else {
         return "Não entendemos a sua dúvida, nos contate no email contato@utfarm.com.br";
     }
+
     //3
-    if (input == "Bom dia") {
+    if (input == "Teste1") {
         return "Bom dia, como podemos te ajudar?";
     } else if (input == "Obrigado") {
         return "A Utfarm agradece o contato!";
     } else {
        return "Não entendemos a sua dúvida, nos contate no email contato@utfarm.com.br";
     }
+
+
     //4
-    if (input == "Bom dia") {
+    if (input == "Teste2") {
         return "Bom dia, como podemos te ajudar?";
     } else if (input == "Obrigado") {
         return "A Utfarm agradece o contato!";
@@ -40,18 +45,10 @@ function getBotResponse(input) {
         return "Não entendemos a sua dúvida, nos contate no email contato@utfarm.com.br";
         }
     //5
-    if (input == "Bom dia") {
+    if (input == "Teste3") {
         return "Bom dia, como podemos te ajudar?";
     } else if (input == "Obrigado") {
         return "A Utfarm agradece o contato!";
     } else {
         return "Não entendemos a sua dúvida, nos contate no email contato@utfarm.com.br";
     }
-
-
-
-
-
-
-
-}
